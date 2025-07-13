36843b7cd5048ee9abcedfccbe1d7bf453b86dd3
tommy <lamchiho27@yahoo.com.hk>
2025-07-13 17:13:27 +0800

    commam

---
+++ 02-named.js
@@ -0,0 +1,11 @@
+// This is a comment
+
+/* declare variables, then assign them some value */
+let base = 10
+
+// let topping = 5 // this line is not executed
+let topping = 40
+
+let sum = base + topping
+
+console.log(sum /* it will be shown on screen */)
